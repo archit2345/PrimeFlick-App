99bdea0177dd42e724e15a5d8aa9902deb87c3eb
archit2345 <jainarchit529@gmail.com>
2025-06-05 23:48:57 +0530

    Removing the reactDev tool for production in queryProvider file

--- src/lib/react-query/QueryProvider.tsx
+++ src/lib/react-query/QueryProvider.tsx
@@ -1,16 +1,15 @@
 import React from "react";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
-//query client it is the core object that stores cache, track quereis, mutations etc and we wrap it in query client provider so that its every childern can use it
+// Query client: core object that stores cache, tracks queries, mutations, etc.
 
 export const QueryProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* Devtools removed for production */}
     </QueryClientProvider>
   );
 };
