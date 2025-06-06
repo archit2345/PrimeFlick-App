f0f76245b8261cd23100711e69f20e86cec63a8d
archit2345 <jainarchit529@gmail.com>
2025-06-06 10:43:57 +0530

    Correct image extension

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -61,7 +61,7 @@ const SigninForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/logo-2.svg" alt="logo" />
+        <img src="/assets/images/logo-2.png" alt="logo" />
 
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
           Log in to your account
